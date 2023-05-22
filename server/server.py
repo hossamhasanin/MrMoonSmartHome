ac6381d16cf169223faab4537eaced36c32f0277
hossamhasanin <kokojake13@gmail.com>
2023-05-22 18:56:31 +0300

    created the server middelware that communicates the app

--- server/server.py
+++ server/server.py
@@ -1,9 +1,13 @@
 import os
 import uuid
 import requests
-from flask import Flask, request
+from flask import Flask, request , send_from_directory
 import torch
 from transformers import pipeline
+from elevenlabs import generate, play, set_api_key , save
+
+set_api_key("9fed84e04fd3592c7a9178adc97c6552")
+
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -15,6 +19,10 @@ pipe = pipeline(
 )
 
 app = Flask(__name__)
+
+@app.route('/ai_audios/<path:filename>')
+def download_file(filename):
+    return send_from_directory('ai_audios', filename)
 
 @app.route('/audio', methods=['POST'])
 def upload_audio():
@@ -28,9 +36,16 @@ def upload_audio():
     rasa_response = requests.post('http://localhost:5005/webhooks/rest/webhook', json={'message': transcript}).json()
     # extract response text from Rasa server response
     response_text = rasa_response[0]['text']
+    audio = generate(
+        text= response_text,
+        voice="Sam"
+    )
+    response_audio_path = str(uuid.uuid4()) + ".mp3"
+    full_path = os.path.join('ai_audios', response_audio_path)
+    save(audio, full_path)
     print("response_text : " + response_text)
     # return JSON response
-    return {'user_transcript': transcript, 'ai_response': response_text}
+    return {'user_transcript': transcript, 'ai_response': response_text , 'ai_audio_path' : 'ai_audios/'+response_audio_path}
 
 @app.route('/message', methods=['POST'])
 def send_message():
@@ -44,4 +59,4 @@ def send_message():
     return {'response': response_text}
 
 if __name__ == '__main__':
-    app.run(debug=True , port=7000)
+    app.run(debug=True , port=7000 , host= '192.168.1.4')
